e7f95291c8bc3e3c978a7b6cf1c0b0db1d982e45
EROS0820 <eros0820@protonmail.com>
2020-09-22 20:27:47 +0300

    added: file receive module

--- upload_server/app/Http/Controllers/UploadController.php
+++ upload_server/app/Http/Controllers/UploadController.php
@@ -19,14 +19,27 @@ class UploadController extends Controller
 
     public function uploadfile(Request $request)
     {
-        print_r("asdfasdf");
-        return(0);
-        $fileName = time().'.'.$request->file->extension();  
+        // return ($request->name);
+        $fileName = time().'.'.$request->zip_file->extension();  
    
-        $request->file->move(public_path('uploads'), $fileName);
-        return back()
-            ->with('success','You have successfully upload file.')
-            ->with('file',$fileName);
+        $request->zip_file->move(public_path('uploads'), $fileName);
+        $target_url = 'http://localhost:8001/api/extract_zip'; // Write your URL here
+        $dir = public_path('uploads').'/'.$fileName; // full directory of the file
+
+        $cFile = curl_file_create($dir);
+        $post = array('file'=> $cFile); // Parameter to be sent
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $target_url);
+        curl_setopt($ch, CURLOPT_POST,1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $result=json_decode(curl_exec($ch));
+        curl_close ($ch);
+        // return back()
+        //     ->with('success','You have successfully upload file.')
+        //     ->with('file',$fileName);
+        return ("success");
    
     }
 }
