83d2b6c5d58d4f3f16b2becb71efdb258d1862a1
EROS0820 <eros0820@protonmail.com>
2020-09-22 19:11:26 +0300

    added: complete whole react app

--- upload_front/src/App.js
+++ upload_front/src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React,{Component} from 'react'; 
 
-export default App;
+class App extends Component { 
+
+	state = { 
+    selectedFile: null
+	}; 
+
+	onFileChange = event => { 
+  	this.setState({ selectedFile: event.target.files[0] }); 
+	}; 
+	
+	onFileUpload = () => { 
+    if (!this.state.selectedFile || this.state.selectedFile.type != "application/x-zip-compressed") { 
+      window.alert("Please fill the zip file");
+      return;
+    }
+    const formData = new FormData(); 
+  
+    formData.append( 
+      "zip_file", 
+      this.state.selectedFile, 
+      this.state.selectedFile.name 
+    ); 
+    axios.post(process.env.REACT_APP_END_POINT +"/api/uploadfile", formData); 
+	}; 
+	
+	// File content to be displayed after 
+	// file upload is complete 
+	fileData = () => { 
+	
+	if (this.state.selectedFile) { 
+		
+		return ( 
+		<div> 
+			<h2>File Details:</h2> 
+			<p>File Name: {this.state.selectedFile.name}</p> 
+			<p>File Type: {this.state.selectedFile.type}</p> 
+			<p> 
+			Last Modified:{" "} 
+			{this.state.selectedFile.lastModifiedDate.toDateString()} 
+			</p> 
+		</div> 
+		); 
+	} else { 
+		return ( 
+		<div> 
+			<br /> 
+			<h4>Choose before Pressing the Upload button</h4> 
+		</div> 
+		); 
+	} 
+	}; 
+	
+	render() { 
+	
+	return ( 
+		<div> 
+			<h3> 
+			File Upload using React! 
+			</h3> 
+			<div> 
+				<input type="file" accept=".zip" onChange={this.onFileChange} /> 
+				<button onClick={this.onFileUpload}> 
+				Upload! 
+				</button> 
+			</div> 
+		{this.fileData()} 
+		</div> 
+	); 
+	} 
+} 
+
+export default App; 
