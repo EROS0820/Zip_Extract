ea242466a6fd7203dae2cbf58e4e002cd28731e6
EROS0820 <eros0820@protonmail.com>
2020-09-22 21:19:49 +0300

    added:complete

--- upload_server/app/Http/Controllers/UploadController.php
+++ upload_server/app/Http/Controllers/UploadController.php
@@ -23,9 +23,8 @@ class UploadController extends Controller
         $fileName = time().'.'.$request->zip_file->extension();  
    
         $request->zip_file->move(public_path('uploads'), $fileName);
-        $target_url = 'http://localhost:8001/api/extract_zip'; // Write your URL here
-        $dir = public_path('uploads').'/'.$fileName; // full directory of the file
-
+        $target_url = 'http://localhost:8002/api/extract_zip'; // Write your URL here
+        $dir = public_path('uploads').'\\'.$fileName; // full directory of the file
         $cFile = curl_file_create($dir);
         $post = array('file'=> $cFile); // Parameter to be sent
 
@@ -34,12 +33,12 @@ class UploadController extends Controller
         curl_setopt($ch, CURLOPT_POST,1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $result=json_decode(curl_exec($ch));
-        curl_close ($ch);
+        $result=curl_exec($ch);
+        // curl_close ($ch);
         // return back()
         //     ->with('success','You have successfully upload file.')
         //     ->with('file',$fileName);
-        return ("success");
+        return ($result);
    
     }
 }
